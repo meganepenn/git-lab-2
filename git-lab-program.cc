c2823f7a9734d5062857379a646108f16ceef9a1
Megan <mp403422@ohio.edu>
2024-01-24 17:28:55 -0500

    lab

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Megan Penn
+ *        Date: 01-24-2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
